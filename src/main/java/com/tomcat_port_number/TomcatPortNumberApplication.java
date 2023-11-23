86851ae8832934d73912c43e4129e0571513c154
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-11-22 20:27:26 -0600

    starting project tomcar port number

---
+++ src/main/java/com/tomcat_port_number/TomcatPortNumberApplication.java
@@ -0,0 +1,13 @@
+package com.tomcat_port_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatPortNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatPortNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomcat_port_number/TomcatPortNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomcat_port_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatPortNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
